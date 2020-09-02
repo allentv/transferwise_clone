e35b689e5c79a97b77a39190232cc43ff407604c
Allen Thomas Varghese <allentv4u@gmail.com>
2020-09-02 21:13:33 +0100

    Updates

--- android/app/src/main/kotlin/com/clone/allen/builds/transferwise/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.clone.allen.builds.transferwise
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/transferwise_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.transferwise_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
